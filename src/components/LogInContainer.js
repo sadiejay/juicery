7fac034038a82ae5d72781026b77d8a486c95891
Sadie <19538219+sadiejay@users.noreply.github.com>
2023-01-16 21:23:18 -0600

    Adds HTML for Log In component

--- src/components/LogInContainer.js
+++ src/components/LogInContainer.js
@@ -1,5 +1,37 @@
+import "./LogIn.scss";
+
 function LogInContainer() {
-  return <h2>Log In</h2>;
+  return (
+    <section className="LogInContainer">
+      <div className="LogInContent">
+        <h2>Welcome!</h2>
+        <p className="signUpP">
+          New to Juicerie?{" "}
+          <a href="#" rel="noopener noreferrer" target="_blank">
+            Sign up
+          </a>
+        </p>
+
+        <form className="LogInContent--form">
+          <input
+            type="text"
+            name="email"
+            id="input--email"
+            placeholder="email"
+          ></input>
+          <input
+            type="text"
+            name="password"
+            id="input--password"
+            placeholder="password"
+          ></input>
+
+          <a href="#" target='_blank' rel="noopener noreferrer" className="forgotP">forgot password?</a>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default LogInContainer;
